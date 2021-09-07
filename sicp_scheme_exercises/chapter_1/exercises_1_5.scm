0e1474f56d6871c66652e5da5e283c072759c6c2
Sean Watland <51850964+swatland610@users.noreply.github.com>
2021-09-07 15:24:04 +0200

    add file structures for sicp & a few exercises

---
+++ sicp_scheme_exercises/chapter_1/exercises_1_5.scm
@@ -0,0 +1,71 @@
+;Exercise 1
+; Determine Outcomes for following expressions
+
+10 ;prints 10
+(+ 5 3 4) ;prints 12
+(- 9 1) ;prints 8
+(/ 6 2) ;prints 3
+(+ (* 2 4) (- 4 6)) ;prints 6
+
+; definitions
+(define a 3) ;prints name 'a', the value of a = 3
+(define b (+ a 1)) ;prints name 'b', the value of b = 4
+
+(+ a b (* a b)) ;prints 19
+(= a b) ;prints #f (False)
+
+; conditions
+(if (and (> b a) (< b (* a b))) ; so if 4 > 3 AND 4 < (3 * 4)
+    b ; returns this value of 4
+    a) ; under current definitions, this will not run
+
+(cond ((= a 4) 6) ; under current defs, this will not return 6
+      ((= b 4) (+ 6 7 a)) ; under current defs, this will not return 16
+      (else 25)) ; this will be returned
+
+(+ 2 (if (> b a) b a)) ; so b > a is True, so b (4) is returned, then + 2 that value so, 6 is returned
+
+(* (cond ((> a b) a) ; 'a' will not be returned
+         ((< a b) b) ; 'b' will be returned here so (4)
+         (else -1)) ; this will not be returned
+    (+ a 1)) ; so this resolves to be 4, and we multiply the other 4, so 16 is returned. 
+
+
+; Exercise 2
+; Translate a compound expression in the book
+(/ (+ 5 4 
+        (- 2 
+            (- 3 
+                (+ 6
+                    (/ 4 5)
+                )
+            )
+        )
+    )
+    (* 3 
+        (- 6 2)
+        (- 2 7)
+    )
+)
+; this returns -37/150
+
+; Exercise 3
+; I need to define a procedure that 
+;   1. takes 3 numbers as args
+;   2. returns the sum of squares of the two largest numbers
+
+; first, let me define a square & sumSquare functions
+(define (square x) 
+    (* x x))
+
+(define (sumSquare x y)
+    (+ x y))
+
+; now I'll compile the main function
+(define (sumLargestSquare x y z) 
+(cond ((and (<= x y) (<= x z)) (sumSquare (square y) (square z)))
+      ((and (<= y x) (<= y z)) (sumSquare (square x) (square z))) 
+      (else (sumSquare (square x) (square y)))
+    )
+) 
+    
